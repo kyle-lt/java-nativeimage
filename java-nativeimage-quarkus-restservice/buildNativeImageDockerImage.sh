4fc4e07cf061b231e94f03e3faa3f953357062a7
Kyle Tully <kjtully@yahoo.com>
2021-08-03 15:22:41 -0500

    removed unnecessary step for building json configs

--- java-nativeimage-quarkus-restservice/buildNativeImageDockerImage.sh
+++ java-nativeimage-quarkus-restservice/buildNativeImageDockerImage.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mvn clean package
-
-# Keep commented out, this command is to help build json for classes that use reflection 
-#java -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -jar target/quarkus-app/quarkus-run.jar
+mvn clean
 
 mvn package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true
 
