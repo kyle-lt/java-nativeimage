e241c97706f3125f9c9e3741898404b5123a3de4
Kyle Tully <kjtully@yahoo.com>
2021-08-03 14:40:11 -0500

    added comment for json builder command

--- java-nativeimage-quarkus-restservice/buildNativeImageDockerImage.sh
+++ java-nativeimage-quarkus-restservice/buildNativeImageDockerImage.sh
@@ -2,6 +2,7 @@
 
 mvn clean package
 
+# Keep commented out, this command is to help build json for classes that use reflection 
 #java -agentlib:native-image-agent=config-output-dir=src/main/resources/META-INF/native-image -jar target/quarkus-app/quarkus-run.jar
 
 mvn package -Pnative -Dquarkus.native.container-build=true -Dquarkus.container-image.build=true
