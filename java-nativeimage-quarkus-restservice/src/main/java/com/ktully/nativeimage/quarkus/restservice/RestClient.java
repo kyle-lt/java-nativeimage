1b1bcc5caa24925f1a44abfe660b8d20be76e55f
Kyle Tully <kjtully@yahoo.com>
2021-08-03 11:29:24 -0500

    changed downstream endpoint to httpbin.org to remove local dependency

--- java-nativeimage-quarkus-restservice/src/main/java/com/ktully/nativeimage/quarkus/restservice/RestClient.java
+++ java-nativeimage-quarkus-restservice/src/main/java/com/ktully/nativeimage/quarkus/restservice/RestClient.java
@@ -18,14 +18,18 @@ public class RestClient {
 
 	@Inject
     public RestClient() {
-        baseUrl = "http://host.docker.internal:8082";
+        //baseUrl = "http://host.docker.internal:8082";
+		// Changing to httpbin.org to remove dependency of micronaut service
+		baseUrl = "http://httpbin.org";
         client = ClientBuilder.newBuilder()
                 .executorService(executorService)
                 .build();
     }
 
-	CompletionStage<String> get() {
-		return client.target(baseUrl + "/hello").request().rx().get(String.class);
+	CompletionStage<String> get() {		
+		//return client.target(baseUrl + "/hello").request().rx().get(String.class);
+		// Changing to httpbin/get to remove dependency of micronaut service
+		return client.target(baseUrl + "/get").request().rx().get(String.class);
 	}
 
 }
