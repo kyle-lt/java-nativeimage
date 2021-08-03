6a0648738ddc201072de9c3636434d782d38982c
Kyle Tully <kjtully@yahoo.com>
2021-08-03 11:28:44 -0500

    no longer using, instead using OTel SDK Extension AutoConfigure for SDK config

--- java-nativeimage-quarkus-restservice/src/main/java/com/ktully/nativeimage/quarkus/restservice/OtelTracerConfig.java
+++ java-nativeimage-quarkus-restservice/src/main/java/com/ktully/nativeimage/quarkus/restservice/OtelTracerConfig.java
@@ -5,11 +5,8 @@ import java.util.concurrent.TimeUnit;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
+import org.jboss.logging.Logger;
 
-// 0.13.1
-//import io.opentelemetry.exporter.jaeger.JaegerGrpcSpanExporter;
 import io.opentelemetry.exporter.logging.LoggingSpanExporter;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
@@ -18,8 +15,6 @@ import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
-
-// 0.14.1
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
@@ -33,6 +28,9 @@ public class OtelTracerConfig {
 	
 	//@Produces
 	//OpenTelemetrySdk openTelemetrySdk;
+	
+	// Logger
+	private static final Logger logger = Logger.getLogger(OtelTracerConfig.class);
 
 	//public Tracer OtelTracer() throws Exception {
 	public static OpenTelemetry OpenTelemetryConfig() {
@@ -47,14 +45,30 @@ public class OtelTracerConfig {
 	                .setScheduleDelay(100, TimeUnit.MILLISECONDS)
 	                .build();	   
 	        
-		// This was working using OTEL_RESOURCE_ATTRIBUTES env var, but apparently not anymore with 0.15.0
+		// This was working using OTEL_RESOURCE_ATTRIBUTES env var, but apparently not anymore with 0.15.0+
+	    
+	    /*
+	     * Attempting to try OTEL_RESOURCE_ATTRIBUTES again, let's see what happens (see below in SdkTracerProvider instantiation)
+	     */
 		AttributeKey<String> myServiceName = AttributeKey.stringKey("service.name");
 		AttributeKey<String> myServiceNamespace = AttributeKey.stringKey("service.namespace");
 	    Resource serviceNameResource =
 	                Resource.create(Attributes.of(myServiceName, "java-nativeimage-quarkus-restservice",
 	                		myServiceNamespace, "kjt-java-nativeimage"));
 		
-		// ** Create OpenTelemetry SdkTracerProvider
+		// Let's log the OTEL_RESOURCE_ATTRIBUTES env var value, if found
+	    logger.info("#### Logging Env Var OTEL_RESOURCE_ATTRIBUTES, if present ####");
+	    System.getenv().forEach((k, v) -> {	    	
+	    	if (k.toString().equalsIgnoreCase("OTEL_RESOURCE_ATTRIBUTES")) {
+	    		logger.info(k + ":" + v);
+	    	}
+	    });
+	    
+	    // Let's log the Resource Attribute otel.resource.attributes
+	    logger.info("#### Logging System Property otel.resource.attributes, if present ####");
+	    logger.info("otel.resource.attributes:" + System.getProperty("otel.resource.attributes", "DOES_NOT_EXIST"));
+	    
+	    // ** Create OpenTelemetry SdkTracerProvider
 		// Use OTLP & Logging Exporters
 	    // Use Service Name Resource (and attributes) defined above
 		// Use AlwaysOn TraceConfig
@@ -62,7 +76,14 @@ public class OtelTracerConfig {
                 SdkTracerProvider.builder()
                     .addSpanProcessor(spanProcessor) // OTLP
                     .addSpanProcessor(SimpleSpanProcessor.create(new LoggingSpanExporter()))
-                    .setResource(Resource.getDefault().merge(serviceNameResource))
+                    // Attempting to try OTEL_RESOURCE_ATTRIBUTES again, let's see what happens (see above in Resource instantiation)
+                    // export OTEL_RESOURCE_ATTRIBUTES=service.name=derpDoesThisWorkEnvVar
+                    // I am also trying System Property -Dotel.resource.attributes=service.name=derpDoesThisWorkSysProp
+                    // According to [here](https://github.com/open-telemetry/opentelemetry-java/blob/4cb2a5f0a3c96dc9af34c7a744f8ccd44da98344/CHANGELOG.md)
+                    // If the opentelemetry-sdk-extension-autoconfigure package is used, then Resource.getDefault() 
+                    // should use the OTEL_RESOURCE_ATTRIBUTES env var to populate attributes - but it's not working!!!!
+                    //.setResource(Resource.getDefault().merge(serviceNameResource))
+                    .setResource(Resource.getDefault())
                     .setSampler(Sampler.alwaysOn())
                     .build();
 	        
