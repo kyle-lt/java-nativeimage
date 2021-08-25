7e97c0d55e86ea180890ad71fd47a8aef8803fb1
Kyle Tully <kjtully@yahoo.com>
2021-08-25 11:10:11 -0500

    added env vars for manually mapping to otel resource attributes

--- java-nativeimage-quarkus-restservice/jar-run.sh
+++ java-nativeimage-quarkus-restservice/jar-run.sh
@@ -5,6 +5,10 @@ export OTEL_RESOURCE_ATTRIBUTES=service.name=java-nativeimage-quarkus-restservic
 export OTEL_TRACES_EXPORTER=otlp
 export OTEL_EXPORTER_OTLP_ENDPOINT=http://host.docker.internal:4317
 
+# The Autoconfigure SDK Extension isn't working to set Resource Attributes, so manually handling it...
+export OTEL_SERVICE_NAME=java-nativeimage-quarkus-restservice
+export OTEL_SERVICE_NAMESPACE=kjt-java-nativeimage
+
 # Run the jar
 java -jar target/quarkus-app/quarkus*jar
 
