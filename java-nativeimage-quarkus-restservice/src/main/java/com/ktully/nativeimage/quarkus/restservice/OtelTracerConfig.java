102ee3642b6758c104faefaab2da7331c710cea1
Kyle Tully <kjtully@yahoo.com>
2021-08-25 11:41:48 -0500

    using application.properties values to populate Resource Attributes

--- java-nativeimage-quarkus-restservice/src/main/java/com/ktully/nativeimage/quarkus/restservice/OtelTracerConfig.java
+++ java-nativeimage-quarkus-restservice/src/main/java/com/ktully/nativeimage/quarkus/restservice/OtelTracerConfig.java
@@ -3,7 +3,6 @@ package com.ktully.nativeimage.quarkus.restservice;
 import java.util.concurrent.TimeUnit;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Produces;
 
 import org.jboss.logging.Logger;
 
@@ -20,88 +19,76 @@ import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 // OTLP Exporter
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 
 @ApplicationScoped
 public class OtelTracerConfig {
-	
-	//@Produces
-	//OpenTelemetrySdk openTelemetrySdk;
-	
+
+	// @Produces
+	// OpenTelemetrySdk openTelemetrySdk;
+
 	// Logger
 	private static final Logger logger = Logger.getLogger(OtelTracerConfig.class);
 
-	//public Tracer OtelTracer() throws Exception {
+	// Using service resource attributes provided by environment variables
+	@ConfigProperty(name = "otel.service.name")
+	private static String otelServiceName;
+	@ConfigProperty(name = "otel.service.namespace")
+	private static String otelServiceNamespace;
+
+	// public Tracer OtelTracer() throws Exception {
 	public static OpenTelemetry OpenTelemetryConfig() {
-		
-	    // ** Create OTLP gRPC Span Exporter & BatchSpanProcessor **
-		OtlpGrpcSpanExporter spanExporter =
-	            OtlpGrpcSpanExporter.builder()
-	            	.setEndpoint("http://host.docker.internal:4317")
-	            	.setTimeout(2, TimeUnit.SECONDS).build();
-	        BatchSpanProcessor spanProcessor =
-	            BatchSpanProcessor.builder(spanExporter)
-	                .setScheduleDelay(100, TimeUnit.MILLISECONDS)
-	                .build();	   
-	        
-		// This was working using OTEL_RESOURCE_ATTRIBUTES env var, but apparently not anymore with 0.15.0+
-	    
-	    /*
-	     * Attempting to try OTEL_RESOURCE_ATTRIBUTES again, let's see what happens (see below in SdkTracerProvider instantiation)
-	     */
+
+		// ** Create OTLP gRPC Span Exporter & BatchSpanProcessor **
+		OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.builder()
+				.setEndpoint("http://host.docker.internal:4317").setTimeout(2, TimeUnit.SECONDS).build();
+		BatchSpanProcessor spanProcessor = BatchSpanProcessor.builder(spanExporter)
+				.setScheduleDelay(100, TimeUnit.MILLISECONDS).build();
+
+		/*
+		 * Attempting to try OTEL_RESOURCE_ATTRIBUTES again, let's see what happens (see
+		 * below in SdkTracerProvider instantiation)
+		 */
 		AttributeKey<String> myServiceName = AttributeKey.stringKey("service.name");
 		AttributeKey<String> myServiceNamespace = AttributeKey.stringKey("service.namespace");
-	    Resource serviceNameResource =
-	                Resource.create(Attributes.of(myServiceName, "java-nativeimage-quarkus-restservice",
-	                		myServiceNamespace, "kjt-java-nativeimage"));
-		
+		Resource serviceNameResource = Resource
+				.create(Attributes.of(myServiceName, otelServiceName, myServiceNamespace, otelServiceNamespace));
+
 		// Let's log the OTEL_RESOURCE_ATTRIBUTES env var value, if found
-	    logger.info("#### Logging Env Var OTEL_RESOURCE_ATTRIBUTES, if present ####");
-	    System.getenv().forEach((k, v) -> {	    	
-	    	if (k.toString().equalsIgnoreCase("OTEL_RESOURCE_ATTRIBUTES")) {
-	    		logger.info(k + ":" + v);
-	    	}
-	    });
-	    
-	    // Let's log the Resource Attribute otel.resource.attributes
-	    logger.info("#### Logging System Property otel.resource.attributes, if present ####");
-	    logger.info("otel.resource.attributes:" + System.getProperty("otel.resource.attributes", "DOES_NOT_EXIST"));
-	    
-	    // ** Create OpenTelemetry SdkTracerProvider
+		logger.info("#### Logging Env Var OTEL_RESOURCE_ATTRIBUTES, if present ####");
+		System.getenv().forEach((k, v) -> {
+			if (k.toString().equalsIgnoreCase("OTEL_RESOURCE_ATTRIBUTES")) {
+				logger.info(k + ":" + v);
+			}
+		});
+
+		// Let's log the Resource Attribute otel.resource.attributes
+		logger.info("#### Logging System Property otel.resource.attributes, if present ####");
+		logger.info("otel.resource.attributes:" + System.getProperty("otel.resource.attributes", "DOES_NOT_EXIST"));
+
+		// ** Create OpenTelemetry SdkTracerProvider
 		// Use OTLP & Logging Exporters
-	    // Use Service Name Resource (and attributes) defined above
+		// Use Service Name Resource (and attributes) defined above
 		// Use AlwaysOn TraceConfig
-        SdkTracerProvider sdkTracerProvider =
-                SdkTracerProvider.builder()
-                    .addSpanProcessor(spanProcessor) // OTLP
-                    .addSpanProcessor(SimpleSpanProcessor.create(new LoggingSpanExporter()))
-                    // Attempting to try OTEL_RESOURCE_ATTRIBUTES again, let's see what happens (see above in Resource instantiation)
-                    // export OTEL_RESOURCE_ATTRIBUTES=service.name=derpDoesThisWorkEnvVar
-                    // I am also trying System Property -Dotel.resource.attributes=service.name=derpDoesThisWorkSysProp
-                    // According to [here](https://github.com/open-telemetry/opentelemetry-java/blob/4cb2a5f0a3c96dc9af34c7a744f8ccd44da98344/CHANGELOG.md)
-                    // If the opentelemetry-sdk-extension-autoconfigure package is used, then Resource.getDefault() 
-                    // should use the OTEL_RESOURCE_ATTRIBUTES env var to populate attributes - but it's not working!!!!
-                    //.setResource(Resource.getDefault().merge(serviceNameResource))
-                    .setResource(Resource.getDefault())
-                    .setSampler(Sampler.alwaysOn())
-                    .build();
-	        
-	    // ** Create OpenTelemetry SDK **
+		SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder().addSpanProcessor(spanProcessor) // OTLP
+				.addSpanProcessor(SimpleSpanProcessor.create(new LoggingSpanExporter()))
+				.setResource(Resource.getDefault().merge(serviceNameResource))
+				// .setResource(Resource.getDefault())
+				.setSampler(Sampler.alwaysOn()).build();
+
+		// ** Create OpenTelemetry SDK **
 		// Use W3C Trace Context Propagation
-        // Use the SdkTracerProvider instantiated above
-	    OpenTelemetrySdk openTelemetrySdk =
-	            OpenTelemetrySdk.builder()
-                	.setTracerProvider(sdkTracerProvider)
-	            	.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-	                .build();
-	    			//.buildAndRegisterGlobal();  // can/should I use this?  Maybe later...
-	    
-	    
-	    //  ** Create Shutdown Hook **
-	    Runtime.getRuntime().addShutdownHook(new Thread(sdkTracerProvider::shutdown));
-	    
-	    return openTelemetrySdk;
+		// Use the SdkTracerProvider instantiated above
+		OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder().setTracerProvider(sdkTracerProvider)
+				.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance())).build();
+
+		// ** Create Shutdown Hook **
+		Runtime.getRuntime().addShutdownHook(new Thread(sdkTracerProvider::shutdown));
+
+		return openTelemetrySdk;
 
 	}
 
